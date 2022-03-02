3e0965cf9a678989cdc87a6479413b5e9cc009ef
Laeli Nurafiah <72023093+LaeliNurafiah@users.noreply.github.com>
2022-03-02 14:39:31 +0700

    Add files via upload

---
+++ Tugas 1/tugas1.php
@@ -0,0 +1,12 @@
+<?php
+
+ $nama = "Laeli Nurafiah"; 
+ $nim = "20090104";  
+ $kelas = "4B";
+
+ echo "Halo! Perkenalkan Saya,\n";
+ echo "Nama     : $nama\n";
+ echo "Nim      : $nim\n";
+ echo "Kelas    : $kelas\n";
+
+?>
